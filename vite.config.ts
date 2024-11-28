8d3ee50ff64b9221f4815f158ca43e35e9441f08
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-11-28 17:13:37 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,32 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./server/app.ts",
-        }
-      : undefined,
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+	build: {
+		rollupOptions: isSsrBuild
+			? {
+					input: './server/app.ts',
+			  }
+			: undefined,
+	},
+
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer],
+		},
+	},
+	plugins: [reactRouter(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		port: 3000,
+		open: true,
+	},
 }));
